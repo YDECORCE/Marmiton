1c4cffdbc9f2bf28e1a43edb9c11d8396abcc178
YDECORCE <y.decorce@codeur.online>
2020-11-17 16:12:53 +0100

    integration 5 recettes

--- vue/recipeoftheday.php
+++ vue/recipeoftheday.php
@@ -1,6 +1,9 @@
 <?php
 ob_start();
-foreach ($fiverecipes as $data){
+$starrecipe = array_slice($fiverecipes,0, 1); 
+$otherrecipes = array_slice($fiverecipes,1, 4); 
+echo"<div class='leftcolumn'>";
+foreach ($starrecipe as $data){
     echo"<div class='card'>
     <h2>".$data['nom_recettes']."</h2>
     <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' background-size:100% auto ; height : 100%; width:100%; display:block'></div>
@@ -10,6 +13,15 @@ foreach ($fiverecipes as $data){
       <p>".$data['recette_recettes']."</p>
 </div>";
 }
+echo'</div><div class="rightcolumn">';
+foreach($otherrecipes as $data){
+
+    echo"<div class='card'>
+      <h3>".$data['nom_recettes']."</h3>
+      <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' background-size:100% auto ; height : 100%; width:100%; display:block'></div>      
+    </div>";
+}
+echo"</div>";
 $loopfiverecipes =  ob_get_clean();
 require_once('template.php');
 ?>
