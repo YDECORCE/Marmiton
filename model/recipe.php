6405f4215d488d5218b90e77bc87c581bed9bc7d
sami39 <hajjisami241@gmail.com>
2020-11-17 12:11:35 +0100

    beforemerge

--- model/recipe.php
+++ model/recipe.php
@@ -1,9 +1,17 @@
 <?php
 require_one('connexion.php');
 
-class Recipe extends connexion
+class Recipe extends Connexion
 {
+    function rondom(){
 
+    
+    $a = 5;
+    $bdd= mysql_query("SELECT * FROM recettes ORDER BY rand() LIMIT $a");
+    while ($row = mysql_fetch_array($requete))   {
+      
+    }
+}
 }
 
 ?>
