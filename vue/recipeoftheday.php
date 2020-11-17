dfa6cadd3ff776a1759868d7b4a607e029b426aa
YDECORCE <y.decorce@codeur.online>
2020-11-17 21:05:55 +0100

    redimensionnement image

--- vue/recipeoftheday.php
+++ vue/recipeoftheday.php
@@ -6,7 +6,7 @@ echo"<div class='leftcolumn'>";
 foreach ($starrecipe as $data){
     echo"<div class='card'>
     <h2>".$data['nom_recettes']."</h2>
-    <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' background-size:100% auto ; height : 100%; width:100%; display:block'></div>
+    <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' background-size:100% auto ; height : 100%; width:auto; display:block'></div>
       <p>".$data['temps_recettes']."<p>
       <p>".$data['difficulte_recettes']."<p>
       <p>".$data['type_recettes']."<p>
@@ -18,7 +18,7 @@ foreach($otherrecipes as $data){
 
     echo"<div class='card'>
       <h3>".$data['nom_recettes']."</h3>
-      <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' background-size:100% auto ; height : 100%; width:100%; display:block'></div>      
+      <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' background-size:100% auto ; height : 100%; width:auto; display:block'></div>      
     </div>";
 }
 echo"</div>";
