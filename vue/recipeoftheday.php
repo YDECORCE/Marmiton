633a0764f4c9af49c01ce16a986d4f50881424ad
sami39 <hajjisami241@gmail.com>
2020-11-17 16:18:34 +0100

    last

--- vue/recipeoftheday.php
+++ vue/recipeoftheday.php
@@ -3,7 +3,8 @@ ob_start();
 foreach ($fiverecipes as $data){
     echo"<div class='card'>
     <h2>".$data['nom_recettes']."</h2>
-    <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' background-size:100% auto ; height : 100%; width:100%; display:block'></div>
+    <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' max-width:100%;
+    ; size: cover; display:block'></div>
       <p>".$data['temps_recettes']."<p>
       <p>".$data['difficulte_recettes']."<p>
       <p>".$data['type_recettes']."<p>
