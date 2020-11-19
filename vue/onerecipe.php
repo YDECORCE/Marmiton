611a041f5c6b085d059bc8a7d311fb84b9e2d32d
YDECORCE <y.decorce@codeur.online>
2020-11-19 13:28:23 +0100

    integration one recipe

--- vue/onerecipe.php
+++ vue/onerecipe.php
@@ -19,3 +19,37 @@ foreach ($recipeingredient as $data)
     }
 
 ?>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8" />
+        <title>Chef Cuisine</title>
+        <link href="public/css/style.css" rel="stylesheet" /> 
+    </head>
+
+</head>
+<div class='container'>
+    <h2>Titre de la recette</h2>
+    <div class='propimage'>
+    <img src='public/img/pizza.jpg' alt='blabla'>
+    </div>
+    <div class="inforecette">
+        <p>Durée :</p>
+        <p>Type :</p>
+        <p>Difficulté :</p>
+        <p>Coût :</p>
+    </div>
+    <div class="ingredient">
+        <form method='get' action='index.php?action=detail&id=8&'>
+        <label for='nbrepers'>Nombre de Personne</label>
+        <input type="range" name='nbrpers' value="4" min="1" max="12" id='nbrepers' oninput="this.nextElementSibling.value = this.value">
+        <output>4</output>
+        <button type='submit' name='calcul' value='calc'>Calculer</button>
+        </form>
+        <p>ingrédient 1</p>
+        <p>ingrédient 1</p>
+        <p>ingrédient 1</p>
+    </div>
+    <div class="recette">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, quasi? Numquam, autem quas eaque vel iusto cum enim fugit iste consequatur neque facere voluptatibus velit harum nobis, voluptatum placeat dolores?</p>
+    </div>
+</div>
