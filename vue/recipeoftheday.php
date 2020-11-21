65ad158049b89558ff4e64ac341b599f89fafb8e
YDECORCE <y.decorce@codeur.online>
2020-11-21 15:25:47 +0100

    intregartion header

--- vue/recipeoftheday.php
+++ vue/recipeoftheday.php
@@ -2,13 +2,13 @@
 ob_start();
 $starrecipe = array_slice($fiverecipes,0, 1); 
 $otherrecipes = array_slice($fiverecipes,1, 4); 
-echo"<div class='leftcolumn'>";
+echo"<div class='container'><div class='row w-100'><div class='col-12 col-sm-8>";
 foreach ($starrecipe as $data){
     $text=str_replace ("/*","</p><p>",$data['recette_recettes']);
     $text=substr($text,0,149).'...';
     echo"<div class='card'>
     <a href='index.php?action=detail&id=".$data['id_recettes']."'>".$data['nom_recettes']."</a>
-    <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' ; width:100%;height :600px; display:block'></div>
+    <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' ; '></div>
     <div class='inforecette'>
         <p>Durée : ".$data['temps_recettes']."</p>
         <p>Type : ".$data['type_recettes']."</p>
@@ -18,7 +18,7 @@ foreach ($starrecipe as $data){
       <p>".$text."</p>
 </div>";
 }
-echo'</div><div class="rightcolumn">';
+echo"</div><div class='col-12 col-sm-4>";
 foreach($otherrecipes as $data){
 
     echo"<div class='card'>
--- vue/template.php
+++ vue/template.php
@@ -1,38 +1,22 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="fr">
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>Chef Cuisine</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
-  </script>
-  <link href="public/css/style.css" rel="stylesheet" />
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Homenaje&display=swap" rel="stylesheet">
-
-
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+<link href="public/css/style.css" rel="stylesheet" />
+  <title>A l'ardoise</title>
 </head>
-
 <body>
-  <header>
-
-
-    <div class="header" style="height:700px">
-      <video autoplay loop muted>
-        <source src="public/img/Food.mp4" type="video/mp4" style=width:500vw;> </video>
-      <!--Navbar-->
-      <nav class="navbar navbar-expand-lg bg-dark ">
+  
+<header>
+       <!--Navbar-->
+      <nav class="navbar navbar-expand-lg">
 
         <!-- Navbar brand -->
-        <a class="navbar-brand" href="index.php"><img src="public/img/chef.png" width="50" height="60" alt=""></a>
+        <a class="navbar-brand mt-5" href="index.php"><h1>A l'ardoise, ce soir...</h1></a>
 
         <!-- Collapse button -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
@@ -41,49 +25,50 @@
         </button>
 
         <!-- Collapsible content -->
-        <div class="collapse navbar-collapse" id="basicExampleNav">
+        <div class="collapse navbar-collapse menu w-100 d-flex justify-content-between" id="basicExampleNav">
 
           <!-- Links -->
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
+          <ul class="navbar-nav d-flex justify-content-between" style="width:50%">
+            <!-- <li class="nav-item active">
               <a class="nav-link" href="index.php">Accueil
                 <span class="sr-only">(current)</span>
               </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="index.php?action=all">Nos Recettes </a>
+            </li> -->
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php?action=all">Nos Recettes
+              <span class="sr-only">(current)</span>
+             </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Recherche par ingrédient</a>
             </li>
-
-
           </ul>
           <!-- Links -->
 
           <form method="GET" class="form-inline">
-            <div class="md-form my-0">
-              <input class="form-control mr-sm-2" name="text" placeholder="Search" aria-label="Search">
-              <button class="btn btn-success" type="submit" name="action" value="rechercher">valider</button>
-            </div>
+              <div class="input-group mb-3">
+                  <input type="text" class="form-control" placeholder="Rechercher une recette"  name="text" aria-label="Recipient's username" aria-describedby="button-addon2">
+                  <div class="input-group-append">
+                    <button class="btn btn-light" type="submit" name="action" value="rechercher" id="button-addon2">valider</button>
+                  </div>
+               </div>
           </form>
         </div>
+      </nav>
+
         <!-- Collapsible content -->
-
+       
 </header>
-
-        <div class="rowe">
-          <?php
+<?php
     if(!empty($loopfiverecipes)){echo $loopfiverecipes;}
     if(!empty($loopallrecipes)){echo $loopallrecipes;}
     if(!empty($onerecipe)){echo $onerecipe;}
     if(!empty($searchrecipe)){echo $searchrecipe;}
     if(!empty($searcherror)){echo $searcherror;}
     
+    
     ?>
-        </div>
-
-        <div class="footer">
+<div class="footer">
           <h2>Footer</h2>
         </div>
 </body>
