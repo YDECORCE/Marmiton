944d1ac028e0bdbb4bdf992ed30510bde7fbd09c
YDECORCE <y.decorce@codeur.online>
2020-11-22 14:42:48 +0100

    ajout image ingredients

--- vue/recipeoftheday.php
+++ vue/recipeoftheday.php
@@ -8,7 +8,7 @@ foreach ($starrecipe as $data){
     $text=substr($text,0,149).'...';
     echo"<div class='card'>
     <a href='index.php?action=detail&id=".$data['id_recettes']."'>".$data['nom_recettes']."</a>
-    <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." '></div>
+    <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' width:100%'></div>
     <div class='info'>
         <p>Durée : ".$data['temps_recettes']."</p>
         <p>Difficulté : ".$data['difficulte_recettes']."</p>
@@ -16,12 +16,13 @@ foreach ($starrecipe as $data){
     </div>
     </div>";
 }
+
 echo"</div><div class='col-12 col-sm-4'>";
 foreach($otherrecipes as $data){
 
     echo"<div class='card'>
       <a href='index.php?action=detail&id=".$data['id_recettes']."'>".$data['nom_recettes']."</a>
-      <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' width:100%; display:block'></div>      
+      <div class='fakeimg'><img src=".$data['image_recettes']." alt=".$data['nom_recettes']." style=' width:100%'></div>      
     </div>";
 }
 echo"</div>";
