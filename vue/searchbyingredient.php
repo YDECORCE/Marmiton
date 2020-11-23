9805808b360c6b76c764394c5afb67e2b32e0c95
YDECORCE <y.decorce@codeur.online>
2020-11-23 14:24:04 +0100

    debut integration search ingredient

--- vue/searchbyingredient.php
+++ vue/searchbyingredient.php
@@ -1,5 +1,15 @@
+
+
 <?php
 var_dump($letters)
 
 // prevoir un if sur varaible ob_start pour afficher le résultat de la recherche
+
+echo'<div class="container">
+    <div class="row">
+        <div class="letter">';
+        foreach ($letters as $data){
+            echo'<a href="index.php?action=searchingredient&letter='.data['letter'].'> '.data['letter'].' -</a>';
+        }
+        echo'</div></div></div>';
 ?>
